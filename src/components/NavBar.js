8d9129a8438afbab82cf972815c85a7381f29b37
Anders Ganander <anders.ganander@gmail.com>
2024-07-03 15:35:10 +0000

    Add post functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,10 +11,13 @@ import {
 
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser()
   const setCurrentUser = useSetCurrentUser()
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -79,7 +82,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar expand="md" className={styles.NavBar} fixed="top" >
+    <Navbar expand="{expanded}" className={styles.NavBar} fixed="top" >
         <Container>
           <NavLink to='/'>
             <Navbar.Brand>
@@ -87,7 +90,10 @@ const NavBar = () => {
             </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle 
+        ref={ref}
+        onClick={() => setExpanded(!expanded)}
+        aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto text-left">
             <NavLink className={styles.NavLink}
