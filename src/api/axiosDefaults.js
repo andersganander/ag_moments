9d698faacdced7c7e313ba88ee606a44b8e6d3a7
Anders Ganander <anders.ganander@gmail.com>
2024-07-03 08:08:21 +0000

    Add logout functionality

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,5 @@ axios.defaults.baseURL = 'https://ag-drf-api-69137997df7c.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.withCredentials = true
 
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
