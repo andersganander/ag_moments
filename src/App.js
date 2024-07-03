1f7c9489948bf7ee5af5097f73e50d697b1db4f0
Anders Ganander <anders.ganander@gmail.com>
2024-07-03 18:52:08 +0000

    Add basic post page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './api/axiosDefaults.js';
 import SignUpForm from "./pages/auth/SignUpForm.js";
 import SignInForm from "./pages/auth/SignInForm.js";
 import PostCreateForm from "./pages/posts/PostCreateForm.js";
+import PostPage from "./pages/posts/PostPage.js";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/signin" element={<SignInForm />} />
         <Route path="/signup" element={<SignUpForm /> } />
         <Route path="/posts/create" element={<PostCreateForm /> } />
+        <Route path="/posts/:id" element={<PostPage />} />
         <Route path="*" element={<p>Page not found!</p>} />
       </Routes>
       </Container>
